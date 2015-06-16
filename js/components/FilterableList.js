fba7480ae5d1127ff8672ff77fb42e5a5a160032
Alexey Simonenko <dwarfman@gmail.com>
2015-06-16 19:05:12 +0300

    Update translation

--- js/components/FilterableList.js
+++ js/components/FilterableList.js
@@ -43,7 +43,7 @@ let FilterableListItem = React.createClass({
                     </h3>
                     <div className="item__instructions">
                         <div dangerouslySetInnerHTML={{__html: this.props.instructions}}></div>
-                        <button className="button item__button" onClick={this.props.resetQuery}>Попробовать ещё раз</button>
+                        <button className="button item__button" onClick={this.props.resetQuery}>Искать снова</button>
                         <button className="button item__button" onClick={this.toggle}>Закрыть</button>
                     </div>
                 </li>
