002115d138644aca63284c9040eb30c93cb28651
Alexey Simonenko <dwarfman@gmail.com>
2015-06-16 19:08:30 +0300

    Fix footer columns

--- js/components/Footer.js
+++ js/components/Footer.js
@@ -5,13 +5,13 @@ let Footer = React.createClass({
         return (
             <footer className="footer row">
                 <div className="wrapper">
-                    <div className="column-4">
+                    <div className="column-4 ms-fw">
                         <h3>Автор</h3>
                         <p>First Aid Git&nbsp;&mdash; проект <a href="http://www.ricardofilipe.com">Рикардо Магальяйнша</a>.</p>
                         <p>Перевод&nbsp;&mdash; <a href="https://htmlacademy.ru">HTML&nbsp;Academy</a>.</p>
                         <p><a href="#/about">Прочитать ещё</a> о&nbsp;проекте.</p>
                     </div>
-                    <div className="column-3">
+                    <div className="column-3 ms-fw">
                         <h3>Другие ресурсы</h3>
                         <ul>
                             <li><a href="https://github.com/k88hudson/git-flight-rules">Путеводитель по Git</a></li>
@@ -20,7 +20,7 @@ let Footer = React.createClass({
                             <li><a href="https://try.github.io/levels/1/challenges/1">Школа GitHub</a></li>
                         </ul>
                     </div>
-                    <div className="column-3">
+                    <div className="column-3 ms-fw">
                         <h3>Благодарности</h3>
                         Поисковый алгоритм&nbsp;&mdash; <a href="http://kiro.me/projects/fuse.html">Fuse.js</a>, а
                         конвертер Markdown&nbsp;&mdash; <a href="https://github.com/showdownjs/showdown">Showdown.js</a>
